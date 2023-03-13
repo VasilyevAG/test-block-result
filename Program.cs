b4043e547a693398cbff5b985397dac9e32ce91f
Alexandr <ftr15@yandex.ru>
2023-03-13 12:41:49 +0700

    2nd commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if (array1[i].Length <= 3)
+        {
+        array2[k] = array1[i];
+        k++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+string[] array1 = new string[] {"hello", "world", "!", "no", "more", "than", "3", "bit"};
+string[] array2 = new string[array1.Length];
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
